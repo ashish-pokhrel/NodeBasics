fc988d5c26aa0172e4e2157d5afc0d4822669b84
ashish-pokhrel <ashishpokhrel45@gmail.com>
2022-05-10 14:15:45 -0500

    SImple calculator

--- SimpleCalculator/index.js
+++ SimpleCalculator/index.js
@@ -9,7 +9,35 @@ app.get("/", (req, res) => {
 });
 
 app.post("/", (req, res) => {
-    res.send(req.body.n1);
+    console.log(req.body);
+    const selectedValue = req.body.selectCalMethod;
+    const valueOne = Number(req.body.n1);
+    const valueTwo = Number(req.body.n2);
+    let returnValue = "";
+    console.log(valueOne);
+    console.log(valueTwo);
+    switch (selectedValue) {
+        case 'add':
+            returnValue = valueOne + valueTwo;
+            break;
+        case 'subtract':
+            returnValue = valueOne - valueTwo;
+            break;
+        case 'multiply':
+            returnValue = valueOne * valueTwo;
+            break;
+        case 'divide':
+            returnValue = valueOne / valueTwo;
+            break;
+
+    }
+    res.send(`
+    <div>
+    <p>The final result is: ${returnValue} </p> <br>
+
+    <a href ="/">Another Calculation </a>
+    </div>    `
+    );
 });
 
 app.listen(8000, () => { console.log("port listing") });
